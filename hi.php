8bcd2dae391feb1ba93600c4a646db7cbaad1c1b
Hussain Moulana <halihussain616@gmail.com>
2021-12-15 03:26:29 +0530

    Initial Commit

---
+++ hi.php
@@ -0,0 +1,5 @@
+<a href=mailto:dc.vs.marvel000@gmail.com.com?subject="HTML link"><i class="fa fa-envelope" style=" font-size:15px">dc.vs.marvel000@gmail.com</a>
+
+<a href=mailto:dc.vs.marvel000@gmail.com?subject="HTML link target="_blank"><i class="fa fa-envelope" style=" font-size:15px"></i>dc.vs.marvel000@gmail.com</a>
+
+<a href=mailto:dc.vs.marvel000@gmail.com?subject><i class="fa fa-envelope" style=" font-size:15px"></i>dc.vs.marvel000@gmail.com</a>
